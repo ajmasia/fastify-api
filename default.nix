1e780dd30d222067ac52043f2a10f968a0359b05
Antonio José Masiá <email@antoniomasia.com>
2023-02-24 16:23:26 +0100

    initialize project

---
+++ default.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  name = "node-env";
+  buildInputs = with pkgs; [
+    nodejs-16_x
+    lolcat
+    # other dependencies
+  ];
+  shellHook = ''
+    clear 
+    echo "Welcome to the $(node --version) environment!" | lolcat
+    echo "****************************************"
+    echo -e "https://nodejs.org/docs/latest-v16.x/api/"
+    echo -e "https://nixos.wiki/wiki/Node.js"
+    echo -e "https://nixos.wiki/wiki/Development_environment_with_nix-shell#direnv"
+    echo "*********************************************************************"
+  '';
+}
